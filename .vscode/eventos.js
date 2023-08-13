7faf474076425054b6122252789b8a07594a5505
VIRIT9 <virstar9@gmail.com>
2023-08-13 20:39:06 -0300

    Solucion

--- .vscode/eventos.js
+++ .vscode/eventos.js
@@ -1,10 +1,14 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
 
-    const div = document.getElementById("boton")
+    const div = document.getElementById("div")
     const button = document.getElementById("button")
-    
-    div.addEventListener("click",()=>{
-        alert("Hola! Soy el Div")
-    })
 
+        div.addEventListener("click", ()=> {
+        alert("Hola! Soy el div");
+        })
+
+        button.addEventListener("click", function(e) {
+        alert("Hola!");
+        e.stopPropagation()
+        })
 })
