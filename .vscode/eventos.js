bef33971d7879e9733c8e0d7d8c772b01a3c52bf
VIRIT9 <virstar9@gmail.com>
2023-08-13 11:53:09 -0300

    Datos finales

---
+++ .vscode/eventos.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    const div = document.getElementById("boton")
+    const button = document.getElementById("button")
+    
+    div.addEventListener("click",()=>{
+        alert("Hola! Soy el Div")
+    })
+
+})
